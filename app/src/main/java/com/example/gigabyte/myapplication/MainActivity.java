f6cbdcf1dc3bb125e284e571091f6dedf493b9dc
zulvrama <zulvrama@gmail.com>
2016-10-31 16:04:48 +0700

    tes

---
+++ app/src/main/java/com/example/gigabyte/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gigabyte.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(res.layout.activity_main);
+    }
+}
